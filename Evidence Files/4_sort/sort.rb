bdeb0b0c7fcf9bcff095d7c2b499760db2ca5422
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-11-20 10:21:03 +0000

    commit amends

--- Evidence Files/4_sort/sort.rb
+++ Evidence Files/4_sort/sort.rb
@@ -5,5 +5,10 @@ users = {
     :home_town => "Inverness",
   },
 }
+# pseudocode:
+# takes array of lottery_numbers within hash James within hash users
+# loops round lottery_numbers array and sorts into acending order with .sort
+# takes first value of sorted lottery_numbers array with .first
+# puts first value of sorted lottery_numbers array with p
 
 p users["James"][:lottery_numbers].sort.first
